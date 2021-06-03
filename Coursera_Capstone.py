e755a8c24d764ef5b243f421a1d36f6efbe7a935
Kumardipta Sarkar <sarkar.kumardipta@gmail.com>
2021-06-03 22:50:22 +0530

    First part of assignment, first commit

---
+++ Coursera_Capstone.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Capstone Project
+# 
+# ### This Porject is the part of the final assignment for the *IBM Data Science Professional Certification*.
+# 
+# This is also a separate course by itself, titled **'Applied Data Science Capstone'**
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+print('Hello Capstone Project Course!')
+
+
+# In[ ]:
+
+
+
+
